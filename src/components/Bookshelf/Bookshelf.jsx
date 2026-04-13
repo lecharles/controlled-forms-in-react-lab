4fb1aef63680a5664f71ed82d77c5ea049148335
lecharles <charleslozano@gmail.com>
2026-04-12 19:19:44 -0700

    step 2: define initial state for books array and newBook object

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
 
 const Bookshelf = () => {
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+
+    const [newBook, setNewBook] = useState({ title: '', author: '' });
+
     return (
         <div className="bookshelfDiv">
             <div className="formDiv">
